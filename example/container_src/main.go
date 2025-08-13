bf4d07a78e5f9beff0814f2fee7bc4a85500cdec
Greg Anders <ganders@cloudflare.com>
2025-08-13 16:01:46 -0500

    Update container code to handle SIGTERM and SIGINT gracefully

--- example/container_src/main.go
+++ example/container_src/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -20,8 +24,36 @@ func errorHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/container", handler)
-	http.HandleFunc("/error", errorHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	stop := make(chan os.Signal, 1)
+
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	router := http.NewServeMux()
+	router.HandleFunc("/", handler)
+	router.HandleFunc("/container", handler)
+	router.HandleFunc("/error", errorHandler)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		log.Printf("Server listening on %s\n", server.Addr)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	<-stop
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Server shutdown successfully")
 }
